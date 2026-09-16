75b40e255fd7da00c6a36d9f24a09a19981c4bd0
afoolhippo <quickment.telecasment@gmail.com>
2026-09-17 06:29:17 +0900

    Add files via upload

--- works-data.js
+++ works-data.js
@@ -22,9 +22,9 @@ const works = [
   },
   {
     title: 'めん処やま志',
-    year: '飯塚にある蕎麦屋「やま志」さんのテーマソングを制作させて頂きました。',
+    year: '2023',
     image: 'works003.jpg',
-    description: '2023',
+    description: '飯塚にある蕎麦屋「やま志」さんのテーマソングを制作させて頂きました。',
     youtube: 'https://youtu.be/ggLOTCnbL14',
     isVisible: true
   },
