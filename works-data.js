19f90f7e724b7f37909f0690cd0b2c37b63dc51d
afoolhippo <quickment.telecasment@gmail.com>
2026-09-19 22:38:20 +0900

    Add files via upload

--- works-data.js
+++ works-data.js
@@ -32,8 +32,7 @@ const works = [
     title: '展望台カフェ',
     year: '2026',
     image: 'works004.jpg',
-    description: '福岡市植物園にある「展望台カフェ」さんのテーマソングを制作させていただきました。子どもたちの笑顔があふれる、明るい雰囲気をイメージした一曲です。
-',
+    description: '福岡市植物園にある「展望台カフェ」さんのテーマソングを制作させていただきました。子どもたちの笑顔があふれる、明るい雰囲気をイメージした一曲です。',
     youtube: 'https://youtu.be/ggLOTCnbL14',
     isVisible: true
   },
